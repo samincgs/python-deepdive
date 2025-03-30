5182eddfa38c2a1b1f659dc14d0d704dbcb88ccb
Samin <samincgs@gmail.com>
2025-03-29 22:53:55 -0400

    learning to send emails in python + postponing regex

--- modules/regex/re_module.py
+++ modules/regex/re_module.py
@@ -1,1 +1,36 @@
 import re
+# COME BACK TO THIS
+
+text_to_search = '''
+abcdefghijklmnopqurtuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+saminhi.com
+
+321-555-4321
+123.555.1234
+123*555*1234
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+pattern = re.compile(r'abc')
+
+matches = pattern.finditer(text_to_search)
+
+for match in matches:
+    print(match)
